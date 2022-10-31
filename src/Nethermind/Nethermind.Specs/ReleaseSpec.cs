02a6225c909c69fe40a87d9ebe6c43e291b45065
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-10-31 14:47:30 +0530

    add verkle tree eip in cancun hardfork

--- src/Nethermind/Nethermind.Specs/ReleaseSpec.cs
+++ src/Nethermind/Nethermind.Specs/ReleaseSpec.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -81,5 +81,6 @@ namespace Nethermind.Specs
         public bool IsEip3651Enabled { get; set; }
         public bool IsEip3855Enabled { get; set; }
         public bool IsEip3860Enabled { get; set; }
+        public bool IsVerkleTreeEipEnabled { get; set; }
     }
 }
