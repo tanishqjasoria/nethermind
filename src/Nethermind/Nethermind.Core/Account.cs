dd6f12b2e8ed0219a86770b191d9f250c17f2343
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-11-01 16:13:20 +0530

    modify account, blockHeader and introduce VerkleWitness

--- src/Nethermind/Nethermind.Core/Account.cs
+++ src/Nethermind/Nethermind.Core/Account.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -24,6 +24,9 @@ namespace Nethermind.Core
         public static Account TotallyEmpty = new();
 
         private static UInt256 _accountStartNonce = UInt256.Zero;
+
+        // todo: change codeHash when this set
+        public byte[]? Code;
 
         /// <summary>
         /// This is a special field that was used by some of the testnets (namely - Morden and Mordor).
@@ -48,6 +51,30 @@ namespace Nethermind.Core
             CodeHash = Keccak.OfAnEmptyString;
             StorageRoot = Keccak.EmptyTreeHash;
             IsTotallyEmpty = Balance.IsZero;
+            CodeSize = 0;
+            Version = UInt256.Zero;
+        }
+
+        public Account(UInt256 balance, UInt256 nonce, Keccak codeHash, UInt256 codeSize, UInt256 version)
+        {
+            Balance = balance;
+            Nonce = nonce;
+            CodeHash = codeHash;
+            StorageRoot = Keccak.EmptyTreeHash;
+            IsTotallyEmpty = Balance.IsZero && Nonce == _accountStartNonce && CodeHash == Keccak.OfAnEmptyString && StorageRoot == Keccak.EmptyTreeHash;
+            CodeSize = codeSize;
+            Version = version;
+        }
+
+        public Account(UInt256 balance, UInt256 nonce, Keccak codeHash)
+        {
+            Balance = balance;
+            Nonce = nonce;
+            CodeHash = codeHash;
+            StorageRoot = Keccak.EmptyTreeHash;
+            IsTotallyEmpty = Balance.IsZero && Nonce == _accountStartNonce && CodeHash == Keccak.OfAnEmptyString && StorageRoot == Keccak.EmptyTreeHash;
+            CodeSize = 0;
+            Version = UInt256.Zero;
         }
 
         private Account()
@@ -57,6 +84,8 @@ namespace Nethermind.Core
             CodeHash = Keccak.OfAnEmptyString;
             StorageRoot = Keccak.EmptyTreeHash;
             IsTotallyEmpty = true;
+            CodeSize = 0;
+            Version = UInt256.Zero;
         }
 
         public Account(in UInt256 nonce, in UInt256 balance, Keccak storageRoot, Keccak codeHash)
@@ -66,6 +95,8 @@ namespace Nethermind.Core
             StorageRoot = storageRoot;
             CodeHash = codeHash;
             IsTotallyEmpty = Balance.IsZero && Nonce == _accountStartNonce && CodeHash == Keccak.OfAnEmptyString && StorageRoot == Keccak.EmptyTreeHash;
+            CodeSize = 0;
+            Version = UInt256.Zero;
         }
 
         private Account(in UInt256 nonce, in UInt256 balance, Keccak storageRoot, Keccak codeHash, bool isTotallyEmpty)
@@ -75,6 +106,8 @@ namespace Nethermind.Core
             StorageRoot = storageRoot;
             CodeHash = codeHash;
             IsTotallyEmpty = isTotallyEmpty;
+            CodeSize = 0;
+            Version = UInt256.Zero;
         }
 
         public bool HasCode => !CodeHash.Equals(Keccak.OfAnEmptyString);
@@ -83,6 +116,8 @@ namespace Nethermind.Core
 
         public UInt256 Nonce { get; }
         public UInt256 Balance { get; }
+        public UInt256 CodeSize { get; }
+        public UInt256 Version { get; }
         public Keccak StorageRoot { get; }
         public Keccak CodeHash { get; }
         public bool IsTotallyEmpty { get; }
@@ -106,6 +141,7 @@ namespace Nethermind.Core
 
         public Account WithChangedCodeHash(Keccak newCodeHash)
         {
+            // TODO: does the code and codeHash match?
             return new(Nonce, Balance, StorageRoot, newCodeHash, IsTotallyEmpty && newCodeHash == Keccak.OfAnEmptyString);
         }
     }
