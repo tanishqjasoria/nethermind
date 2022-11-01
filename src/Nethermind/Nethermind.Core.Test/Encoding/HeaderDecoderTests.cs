cb0e5e5a871d724c503c248d973e66f5cfba0ec7
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-11-01 16:26:47 +0530

    add tests

--- src/Nethermind/Nethermind.Core.Test/Encoding/HeaderDecoderTests.cs
+++ src/Nethermind/Nethermind.Core.Test/Encoding/HeaderDecoderTests.cs
@@ -1,20 +1,21 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
@@ -152,6 +153,63 @@ namespace Nethermind.Core.Test.Encoding
             blockHeader.GasUsed.Should().Be(negativeLong);
             blockHeader.Number.Should().Be(negativeLong);
             blockHeader.GasLimit.Should().Be(negativeLong);
+        }
+
+        [Test]
+        public void Can_encode_decode_with_verkle_proof()
+        {
+            try
+            {
+                byte[] verkleProof =
+                {
+                    121, 85, 7, 198, 131, 230, 143, 90, 165, 129, 173, 81, 186, 89, 19, 191, 13, 107, 197, 120, 243,
+                    229, 224, 183, 72, 25, 6, 8, 210, 159, 31, 3
+                };
+                byte[] key =  {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                };
+
+                byte[] value =  {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2
+                };
+                List<byte[][]> verkleWitness = new();
+                verkleWitness.Add(new[] { key, value });
+                HeaderDecoder.Eip1559TransitionBlock = 0;
+                HeaderDecoder.VerkleTreeTransitionTimestamp = 0;
+                BlockHeader header = Build.A.BlockHeader.WithBaseFee(123).WithVerkleWitness(verkleProof, verkleWitness).TestObject;
+                Rlp rlp = Rlp.Encode(header);
+                BlockHeader blockHeader = Rlp.Decode<BlockHeader>(rlp);
+                blockHeader.BaseFeePerGas.Should().Be(123);
+                blockHeader.VerkleProof.Should().BeEquivalentTo(verkleProof);
+                blockHeader.VerkleWitnesses[0][0].Should().BeEquivalentTo(key);
+                blockHeader.VerkleWitnesses[0][1].Should().BeEquivalentTo(value);
+            }
+            finally
+            {
+                HeaderDecoder.Eip1559TransitionBlock = long.MaxValue;
+                HeaderDecoder.VerkleTreeTransitionTimestamp = long.MaxValue;
+            }
+        }
+
+        [Test]
+        public void Can_encode_decode_with_verkle_proof_empty()
+        {
+            try
+            {
+                HeaderDecoder.Eip1559TransitionBlock = 0;
+                HeaderDecoder.VerkleTreeTransitionTimestamp = 0;
+                BlockHeader header = Build.A.BlockHeader.WithBaseFee(123).WithVerkleWitness(null, null).TestObject;
+                Rlp rlp = Rlp.Encode(header);
+                BlockHeader blockHeader = Rlp.Decode<BlockHeader>(rlp);
+                blockHeader.BaseFeePerGas.Should().Be(123);
+                blockHeader.VerkleProof.Should().BeNull();
+                blockHeader.VerkleWitnesses.Should().BeNull();
+            }
+            finally
+            {
+                HeaderDecoder.Eip1559TransitionBlock = long.MaxValue;
+                HeaderDecoder.VerkleTreeTransitionTimestamp = long.MaxValue;
+            }
         }
     }
 }
